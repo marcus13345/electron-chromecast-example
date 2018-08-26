3af5456c2310b86a6d0607ba79a0426bcf15631f
Marcus <marcus@mandworks.com>
2018-08-26 16:52:24 -0400

    init

---
+++ index.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const electron = require('electron');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+let receiverServer = null;
+
+if (typeof electron === 'string') {
+	spawnElectron()
+	startReceiverServer()
+}
+else electron.app.on('ready', run)
+
+function spawnElectron() {
+	let {spawn} = require('child_process');
+	let filepath = (process.argv[1]);
+	let electronProcess = spawn(electron, [filepath, ...process.argv.slice(2)], {
+		stdio: 'inherit'
+	})
+
+	electronProcess.on('close', _ => {
+		stopReceiverServer();
+	})
+}
+
+function run() {
+	let {BrowserWindow} = electron;
+
+	
+	// console.log(electron)
+	let window = new BrowserWindow({
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js')
+		}
+	});
+
+	window.loadURL(path.join(__dirname, 'index.html'));
+	window.setMenu(null);
+	window.setTitle('Danbooru Explorer');
+	window.webContents.toggleDevTools();
+
+	window.on('close', _ => process.exit(0));
+
+}
+
+function startReceiverServer() {
+	app.get('/receiver.html', (req, res) => {
+		console.log(req.headers);
+		res.statusCode = 200;
+		res.end(fs.readFileSync('Receiver.html'))
+	});
+	receiverServer = app.listen(8080);
+}
+
+function stopReceiverServer() {
+	if(receiverServer !== null)
+		receiverServer.close();
+}
---
+++ preload.js
@@ -0,0 +1,23 @@
+require('electron-chromecast');
+
+castConsole.log = console.log;
+castConsole.info = console.log;
+castConsole.warn = console.log;
+castConsole.error = console.log;
+
+chrome.cast.initialize({
+	receiverListener: _ => {
+	},
+	sessionListener: _ => {
+		console.log(_);
+		debugger;
+	},
+	// sessionRequest: new chrome.cast.SessionRequest('794B7BBF') // hello world
+	sessionRequest: new chrome.cast.SessionRequest('D31696B2') // custom receiver
+	// sessionRequest: new chrome.cast.SessionRequest('7A566E83') // default receiver
+	
+}, _=>{
+	debugger;
+}, _=> {
+	debugger;
+})
